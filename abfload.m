934a88f6dacbb2352207ba4c8a7fe30c0dd26eb3
Noah Pettit <noahpettit@g.harvard.edu>
2017-11-28 01:52:16 -0500

    added optional text display flag
    wanted the option to supress all the info printed on the command line.
    global var flag maybe isnt the best practice, but it requires the least
    editing. find&replace-d all disp() with calls to dispif() subfunction.

--- abfload.m
+++ abfload.m
@@ -50,6 +50,8 @@ function [d,si,h]=abfload(fn,varargin)
 %                                 the platform the data were recorded/shall
 %                                 be read by abfload 'ieee-be' is the 
 %                                 alternative.
+% displayinfo   boolean, true    whether file info will be
+%                                 printed on the screen
 % << OUTPUT VARIABLES <<<
 % NAME  TYPE            DESCRIPTION
 % d                     the data read, the format depending on the record-
@@ -90,7 +92,19 @@ function [d,si,h]=abfload(fn,varargin)
 % -------------------------------------------------------------------------
 %                       PART 1: check of input vars
 % -------------------------------------------------------------------------
-disp(['** ' mfilename])
+% determine if we want to display text
+global abfload_displayflag % unique name just in case existng global var
+vs = varargin;
+vs(~cellfun(@ischar,vs)) = deal({''});
+dfi = find(~cellfun(@isempty,strfind(vs,'displayinfo')));
+if ~isempty(dfi)
+    abfload_displayflag = varargin{dfi+1};
+else
+    abfload_displayflag = 1; % default is true
+end
+
+
+dispif(['** ' mfilename])
 % --- defaults   
 % gap-free
 start=0.0;
@@ -105,10 +119,10 @@ channels='a';
 chunk=0.05;
 machineF='ieee-le';
 verbose=1;
-% if first and only optional input argument is string 'info' the user's
+% if first input argument is string 'info' the user's
 % request is to obtain information on the file (header parameters), so set
 % flag accordingly
-if nargin==2 && ischar(varargin{1}) && strcmp('info',varargin{1})
+if strcmp('info',varargin{1})
   doLoadData=false;
 else
   doLoadData=true;
@@ -135,7 +149,7 @@ end
 % -------------------------------------------------------------------------
 %                       PART 2a: determine abf version
 % -------------------------------------------------------------------------
-disp(['opening ' fn '..']);
+dispif(['opening ' fn '..']);
 [fid,messg]=fopen(fn,'r',machineF);
 if fid == -1
   error(messg);
@@ -447,17 +461,17 @@ else
 end
 if eflag
   fclose(fid);
-  disp('**** available channels:');
-  disp(h.recChNames);
-  disp(' ');
-  disp('**** requested channels:');
-  disp(channels);
+  dispif('**** available channels:');
+  dispif(h.recChNames);
+  dispif(' ');
+  dispif('**** requested channels:');
+  dispif(channels);
   error('at least one of the requested channels does not exist in data file (see above)');
 end
 % display available channels if in info mode
 if ~doLoadData
-  disp('**** available channels:');
-  disp(h.recChNames);
+  dispif('**** available channels:');
+  dispif(h.recChNames);
 end
 
 % gain of telegraphed instruments, if any
@@ -518,7 +532,7 @@ end
 % -------------------------------------------------------------------------
 switch h.nOperationMode
   case 1
-    disp('data were acquired in event-driven variable-length mode');
+    dispif('data were acquired in event-driven variable-length mode');
     if h.fFileVersionNumber>=2.0
       errordlg('abfload currently does not work with data acquired in event-driven variable-length mode and ABF version 2.0','ABF version issue');
     else
@@ -593,11 +607,11 @@ switch h.nOperationMode
     
   case {2,4,5}
     if h.nOperationMode==2
-      disp('data were acquired in event-driven fixed-length mode');
+      dispif('data were acquired in event-driven fixed-length mode');
     elseif h.nOperationMode==4
-      disp('data were acquired in high-speed oscilloscope mode');
+      dispif('data were acquired in high-speed oscilloscope mode');
     else
-      disp('data were acquired in waveform fixed-length mode');
+      dispif('data were acquired in waveform fixed-length mode');
     end
     % extract timing information on sweeps
     if (h.lSynchArrayPtr<=0 || h.lSynchArraySize<=0)
@@ -692,7 +706,7 @@ switch h.nOperationMode
     end
     
   case 3
-    disp('data were acquired in gap-free mode');
+    dispif('data were acquired in gap-free mode');
     % from start, stop, headOffset and h.fADCSampleInterval calculate first point to be read
     %  and - unless stop is given as 'e' - number of points
     startPt=floor(1e6*start*(1/h.fADCSampleInterval));
@@ -717,10 +731,10 @@ switch h.nOperationMode
     end
     tmp=1e-6*h.lActualAcqLength*h.fADCSampleInterval;
     if verbose
-      disp(['total length of recording: ' num2str(tmp,'%5.1f') ' s ~ ' num2str(tmp/60,'%3.0f') ' min']);
-      disp(['sampling interval: ' num2str(h.si,'%5.0f') ' µs']);
+      dispif(['total length of recording: ' num2str(tmp,'%5.1f') ' s ~ ' num2str(tmp/60,'%3.0f') ' min']);
+      dispif(['sampling interval: ' num2str(h.si,'%5.0f') ' µs']);
       % 8 bytes per data point expressed in Mb
-      disp(['memory requirement for complete upload in matlab: '...
+      dispif(['memory requirement for complete upload in matlab: '...
         num2str(round(8*h.lActualAcqLength/2^20)) ' MB']);
     end
     % recording start and stop times in seconds from midnight
@@ -775,7 +789,7 @@ switch h.nOperationMode
         dix(:,2)=dix(:,1)+chunkPtsPerChan-1;
         dix(end,2)=h.dataPtsPerChan;
         if verbose && nChunk
-          disp(['reading file in ' int2str(nChunk) ' chunks of ~' num2str(chunk) ' Mb']);
+          dispif(['reading file in ' int2str(nChunk) ' chunks of ~' num2str(chunk) ' Mb']);
         end
         % do it: if no remainder exists loop through all rows of dix,
         % otherwise spare last row for the lines below (starting with
@@ -824,7 +838,7 @@ switch h.nOperationMode
       end
     end
   otherwise
-    disp('unknown recording mode -- returning empty matrix');
+    dispif('unknown recording mode -- returning empty matrix');
     d=[];
     h.si=[];
 end
@@ -1140,6 +1154,11 @@ if ~isempty(x)
   end
 end
 
+function dispif(str)
+global abfload_displayflag
+if abfload_displayflag
+    disp(str);
+end
 
 
 % 
